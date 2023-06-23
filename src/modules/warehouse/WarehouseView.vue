6e47a501af3a241c4aa754dd4f8299bfe26078f5
azama <a.kurmanov@csi.kz>
2023-06-24 01:28:28 +0600

    create first forms

---
+++ src/modules/warehouse/WarehouseView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="warehouse-wrapper">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+import {watch} from "vue";
+
+export default {
+  name: "WarehouseView",
+  components: {},
+  props: {},
+  data() {
+    return {
+      title: 'This is the main page of warehouse'
+    }
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  mounted() {}
+}
+</script>
+
+<style scoped>
+  .warehouse-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+</style>
